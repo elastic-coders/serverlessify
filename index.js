fcc0258110cf4bc27f43852a632cb7831ab5e878
Nicola Peduzzi <thenikso@gmail.com>
2016-08-24 18:41:53 +0200

    Fix usage

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
 'use strict';
 
 const getObjectPath = require('./lib/utils').getObjectPath;
-const executeLambdaCallback = require('./lib/lambda-callback').executeLambdaCallback;
-const authorizerValidationCallback = require('./lib/authorizer-callback').authorizerValidationCallback;
-const authorizerCheckCallback = require('./lib/authorizer-callback').authorizerCheckCallback;
-const decorateLambdaReqCallback = require('./lib/decorators-callback').decorateLambdaReqCallback;
-const decorateAddCORSCallback = require('./lib/decorators-callback').decorateAddCORSCallback;
+const executeLambdaCallback = require('./lib/lambda-callbacks').executeLambdaCallback;
+const authorizerValidationCallback = require('./lib/authorizer-callbacks').authorizerValidationCallback;
+const authorizerCheckCallback = require('./lib/authorizer-callbacks').authorizerCheckCallback;
+const decorateLambdaReqCallback = require('./lib/decorators-callbacks').decorateLambdaReqCallback;
+const decorateAddCORSCallback = require('./lib/decorators-callbacks').decorateAddCORSCallback;
 
-module.exports = ({ html, authorizers }) => (slsConf, slsHandlers) => {
-  for (let funcId in slsConf.functions) {
-    const funcConf = slsConf.functions[funcId];
-    let func = getObjectPath(funcConf.handler, slsHandlers);
-    const events = (funcConf.events || []).map(e => e.http).filter(e => !!e);
-    for (let e of events) {
-      const authSource = getObjectPath(['authorizer', 'identitySource'], e);
-      const authValidatorExp = getObjectPath(['authorizer', 'identityValidationExpression'], e);
-      const authorizerFunction = (
-        getObjectPath(getObjectPath(['authorizer', 'arn'], e) || '', authorizers) ||
-        getObjectPath(getObjectPath(['authorizer', 'name'], e) || '', slsHandlers)
-      );
-      html(
-        e.method.toLowerCase(),
-        `/${e.path.replace(/\{(.+?)\}/g, ':$1')}`,
-        [
-          decorateLambdaReqCallback(),
-          ...(authorizerFunction ? [
-            authorizerValidationCallback(
-              authSource,
-              authValidatorExp ? new RegExp(authValidatorExp) : null,
-              {
-                regionId: 'express',
-                accountId: 'serverlessify',
-                apiId: `${slsConf.service}-${funcId}`,
-              }
-            ),
-            authorizerCheckCallback(authorizerFunction)
-          ] : []),
-          ...(e.cors ? [decorateAddCORSCallback()] : []),
-          executeLambdaCallback(func),
-        ]
-      )
+// Options:
+// - `html`
+// - `authorizers`
+// - `setCacheEntry`
+// - `getCacheEntry`
+module.exports = function(options) {
+  return function(slsConf, slsHandlers) {
+    for (let funcId in slsConf.functions) {
+      const funcConf = slsConf.functions[funcId];
+      let func = getObjectPath(funcConf.handler, slsHandlers);
+      const events = (funcConf.events || []).map(e => e.http).filter(e => !!e);
+      for (let e of events) {
+        const authSource = getObjectPath(['authorizer', 'identitySource'], e);
+        const authValidatorExp = getObjectPath(['authorizer', 'identityValidationExpression'], e);
+        const authorizerFunction = (
+          getObjectPath(getObjectPath(['authorizer', 'arn'], e) || '', options.authorizers) ||
+          getObjectPath(getObjectPath(['authorizer', 'name'], e) || '', slsHandlers)
+        );
+        options.html(
+          e.method.toLowerCase(),
+          `/${e.path.replace(/\{(.+?)\}/g, ':$1')}`,
+          [
+            decorateLambdaReqCallback(),
+            ...(authorizerFunction ? [
+              authorizerValidationCallback(
+                authSource,
+                authValidatorExp ? new RegExp(authValidatorExp) : null,
+                {
+                  regionId: 'express',
+                  accountId: 'serverlessify',
+                  apiId: `${slsConf.service}-${funcId}`,
+                }
+              ),
+              authorizerCheckCallback(
+                authorizerFunction,
+                options.setCacheEntry,
+                options.getCacheEntry
+              )
+            ] : []),
+            ...(e.cors ? [decorateAddCORSCallback()] : []),
+            executeLambdaCallback(func),
+          ]
+        )
+      }
     }
-  }
+  };
 };
